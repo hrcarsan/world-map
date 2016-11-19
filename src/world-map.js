9e738d5a95dabd10e94eb087f0894fa58f83f29b
Santiago H. Cardona <santiagohecar@gmail.com>
2016-11-19 00:01:46 -0500

    fix: canvas maximum width

--- src/world-map.js
+++ src/world-map.js
@@ -36,8 +36,10 @@ function WorldMap(containerId, options)
 
   this.setOptions(options);
 
+  var maxWidth = this.options.height*2;
+
   this.container        = d3.select("#"+containerId);
-  this.width            = this.options.width;
+  this.width            = maxWidth < this.options.width? maxWidth: this.options.width;
   this.height           = this.options.height;
   this.initialScale     = this.height/Math.PI;
   this.initialTranslate = [this.width/2, this.height/2];
